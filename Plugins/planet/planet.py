28cfbfd705d5891eb2fd470462a80a63931c6879
onerandomusername <genericusername414+git@gmail.com>
2021-08-29 19:23:47 -0400

    refactor tox.ini to .flake8 and pyproject.toml

--- Plugins/planet/planet.py
+++ Plugins/planet/planet.py
@@ -1,8 +1,14 @@
+from __future__ import annotations
+
 import logging
+from typing import TYPE_CHECKING
 
 from discord.ext import commands
-from discord.ext.commands import Context
-from modmail.plugin_helpers import ModmailBot, ModmailLogger, PluginCog
+from modmail.plugin_helpers import PluginCog
+
+if TYPE_CHECKING:
+    from discord.ext.commands import Context
+    from modmail.plugin_helpers import ModmailBot, ModmailLogger
 
 log: ModmailLogger = logging.getLogger(__name__)
 
@@ -17,7 +23,7 @@ class Planet(PluginCog):
     @commands.command()
     async def world(self, ctx: Context) -> None:
         """Tell you what planet you are on."""
-        log.debug("The alien {0} has requested to know what planet they are on.".format(ctx.author))
+        log.debug(f"The alien {ctx.author} has requested to know what planet they are on.")
         await ctx.send("earth")
 
 
